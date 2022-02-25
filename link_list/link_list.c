efe9b21c1ea86ce131ba6a1299603b5fe1a5c165
kmq116 <coderco@foxmail.com>
2022-02-25 23:27:29 +0800

    链表排序

--- link_list/link_list.c
+++ link_list/link_list.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <malloc.h>
 #include <stdlib.h>
+#include <stdbool.h>
 // 链表分类 1 单向链表  2 双向链表
 //  循环链表
 
@@ -12,6 +13,51 @@ typedef struct Node
 
 //  链表插入  待插入元素是  拆分结点  change next 指向
 // 离散 存储 链表
+
+bool isEmpty(pNode L)
+{
+  return L->pNext == NULL;
+}
+
+int Length(pNode L)
+{
+  if (isEmpty(L))
+    return 0;
+  int i = 0;
+  pNode p = L->pNext;
+  while (p != NULL)
+  {
+    i++;
+    p = p->pNext;
+  }
+  return i;
+}
+
+void sort(pNode L)
+{
+  int len = Length(L);
+  int i, j, t;
+  pNode p, q;
+  for (i = 0, p = L->pNext; i < len - 1; i++, p = p->pNext)
+  {
+    for (j = i + 1, q = p->pNext; j < len; j++, q = q->pNext)
+    {
+
+      if (p->data > q->data)
+      {
+        t = p->data;
+        p->data = q->data;
+        q->data = t;
+      }
+    }
+  }
+}
+
+void insert(pNode L, int index, int val) {}
+
+void delete (pNode L, int index, int val)
+{
+}
 
 // 返回一个指针变量
 pNode init()
@@ -54,11 +100,14 @@ pNode init()
   return head;
 }
 
+//  遍历链表
 void traverse(pNode L)
 {
   pNode p = L->pNext;
 
-//  应该判断 p 不指向空 
+  //  应该判断 p 不指向空
+  //  应该判断 p 不指向空
+  //  应该判断 p 不指向空
   while (p != NULL)
   {
     printf("%d\n", p->data);
@@ -70,6 +119,10 @@ int main(void)
 {
   pNode pHead = NULL; // 创建一个链表的指针变量
   pHead = init();
-
+  sort(pHead);
   traverse(pHead);
+
+  printf("%d\n", Length(pHead));
+
+  return 0;
 }
